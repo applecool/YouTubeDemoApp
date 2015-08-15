82d3dd0c4042a547ae3e31b4f21bc94d6abf5fe3
applecool <kmitharsha@gmail.com>
2015-08-15 13:45:25 -0400

    updated YouTubeActivity

--- src/org/shellzero/youtubedemoapp/YouTubeActivity.java
+++ src/org/shellzero/youtubedemoapp/YouTubeActivity.java
@@ -12,83 +12,86 @@ import com.google.android.youtube.player.YouTubePlayer.PlayerStateChangeListener
 import com.google.android.youtube.player.YouTubePlayer.Provider;
 import com.google.android.youtube.player.YouTubePlayerView;
 
-public class YouTubeActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener{
+public class YouTubeActivity extends YouTubeBaseActivity implements
+		YouTubePlayer.OnInitializedListener {
 
 	public static final String GOOGLE_YOUTUBE_API_KEY = "AIzaSyDug1kgMBaxpXl7bbm6ZwrEds66rvO2rEM";
-	
+
 	public static final String YOUTUBE_VIDEO_ID = "R81hicmW2Jw";
-	
-	protected void onCreate(Bundle savedInstanceState){
+
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.youtube_main);
 		YouTubePlayerView youTubePlayView = (YouTubePlayerView) findViewById(R.id.youtube_player);
 		youTubePlayView.initialize(GOOGLE_YOUTUBE_API_KEY, this);
-		
+
 	}
-	
-	public void onInitializationFailure(Provider provider, YouTubeInitializationResult result){
-		Toast.makeText(this, "Cannot initialize Youtube player", Toast.LENGTH_LONG).show();
+
+	public void onInitializationFailure(Provider provider,
+			YouTubeInitializationResult result) {
+		Toast.makeText(this, "Cannot initialize Youtube player",
+				Toast.LENGTH_LONG).show();
 	}
-	
-	public void onInitializationSuccess(Provider provider, YouTubePlayer player, boolean wasRestored){
+
+	public void onInitializationSuccess(Provider provider,
+			YouTubePlayer player, boolean wasRestored) {
 		player.setPlayerStateChangeListener(playerStateChangeListener);
 		player.setPlaybackEventListener(playbackEventListener);
-		
-		if(!wasRestored){
+
+		if (!wasRestored) {
 			player.cueVideo(YOUTUBE_VIDEO_ID);
 		}
-		
+
 	}
-	
-	private PlaybackEventListener playbackEventListener = new PlaybackEventListener(){
-		
-		public void onBuffering(boolean arg0){
-		
+
+	private PlaybackEventListener playbackEventListener = new PlaybackEventListener() {
+
+		public void onBuffering(boolean arg0) {
+
 		}
-		
-		public void onPaused(){
-		
+
+		public void onPaused() {
+
 		}
-		
-		public void onPlaying(){
-		
+
+		public void onPlaying() {
+
 		}
-		public void onSeekTo(int arg0){
-			
+
+		public void onSeekTo(int arg0) {
+
 		}
-		
-		public void onStopped(){
-		
+
+		public void onStopped() {
+
 		}
-		
+
 	};
-	
-	
-	private PlayerStateChangeListener playerStateChangeListener = new PlayerStateChangeListener(){
-		
-		public void onAdStarted(){
-		
+
+	private PlayerStateChangeListener playerStateChangeListener = new PlayerStateChangeListener() {
+
+		public void onAdStarted() {
+
 		}
-		
-		public void onError(ErrorReason arg0){
+
+		public void onError(ErrorReason arg0) {
 		}
-		
-		public void onLoading(){
-			
+
+		public void onLoading() {
+
 		}
-		
-		public void onLoaded(String arg0){
-			
+
+		public void onLoaded(String arg0) {
+
 		}
-		
-		public void onVideoStarted(){
-			
+
+		public void onVideoStarted() {
+
 		}
-		
-		public void onVideoEnded(){
-			
+
+		public void onVideoEnded() {
+
 		}
 	};
 
-	
 }
